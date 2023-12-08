48825aaac1215cec9a3117fd91a44d734966e1cf
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-08 15:24:29 -0600

    Tweaked imports for routers

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const router = express.Router()
+const { Router } = require('express')
 const pool = require('../modules/pool')
+
+const router = Router()
 
 router.get('/', (req, res) => {
   // Add query to get all genres
