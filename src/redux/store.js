b442394551c8a5a55739ea30ccb3327d82aa550a
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-08 15:17:01 -0600

    Formatted code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,38 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
-import axios from 'axios';
+import axios from 'axios'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
+import { put, takeEvery } from 'redux-saga/effects'
 
 // Create the rootSaga generator function
 function* rootSaga() {
-  yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+  yield takeEvery('FETCH_MOVIES', fetchAllMovies)
 }
 
 function* fetchAllMovies() {
   try {
     // Get the movies:
-    const moviesResponse = yield axios.get('/api/movies');
+    const moviesResponse = yield axios.get('/api/movies')
     // Set the value of the movies reducer:
     yield put({
       type: 'SET_MOVIES',
-      payload: moviesResponse.data
-    });
+      payload: moviesResponse.data,
+    })
   } catch (error) {
-    console.log('fetchAllMovies error:', error);
+    console.log('fetchAllMovies error:', error)
   }
 }
 
 // Create sagaMiddleware
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
   switch (action.type) {
     case 'SET_MOVIES':
-      return action.payload;
+      return action.payload
     default:
-      return state;
+      return state
   }
 }
 
@@ -40,9 +40,9 @@ const movies = (state = [], action) => {
 const genres = (state = [], action) => {
   switch (action.type) {
     case 'SET_GENRES':
-      return action.payload;
+      return action.payload
     default:
-      return state;
+      return state
   }
 }
 
@@ -54,9 +54,9 @@ const storeInstance = createStore(
   }),
   // Add sagaMiddleware to our store
   applyMiddleware(sagaMiddleware, logger),
-);
+)
 
 // Pass rootSaga into our sagaMiddleware
-sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga)
 
-export default storeInstance;
+export default storeInstance
