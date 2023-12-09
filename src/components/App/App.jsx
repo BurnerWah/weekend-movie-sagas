e1decd3355b069b60af37c8a27fa9ad71d84ee50
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-09 13:15:59 -0600

    Added CSSVarsProvider to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import { CssVarsProvider } from '@mui/joy'
 import { Route, HashRouter as Router } from 'react-router-dom'
 import DetailsPage from '../DetailsPage/DetailsPage'
 import MovieList from '../MovieList/MovieList'
@@ -5,19 +6,21 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
-        <Route path="/details/:id">
-          <DetailsPage />
-        </Route>
+    <CssVarsProvider defaultMode="system">
+      <div className="App">
+        <h1>The Movies Saga!</h1>
+        <Router>
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
+          <Route path="/details/:id">
+            <DetailsPage />
+          </Route>
 
-        {/* Add Movie page */}
-      </Router>
-    </div>
+          {/* Add Movie page */}
+        </Router>
+      </div>
+    </CssVarsProvider>
   )
 }
 
