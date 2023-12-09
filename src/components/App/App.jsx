d4f7d00afc05f3a9906f34d3b68c211c04f20499
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-09 13:46:47 -0600

    Added sheet and typography components to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { CssVarsProvider } from '@mui/joy'
+import { CssVarsProvider, Sheet, Typography } from '@mui/joy'
 import { Route, HashRouter as Router } from 'react-router-dom'
 import DetailsPage from '../DetailsPage/DetailsPage'
 import MovieList from '../MovieList/MovieList'
@@ -7,19 +7,21 @@ import './App.css'
 function App() {
   return (
     <CssVarsProvider defaultMode="system">
-      <div className="App">
-        <h1>The Movies Saga!</h1>
-        <Router>
-          <Route path="/" exact>
-            <MovieList />
-          </Route>
-          <Route path="/details/:id">
-            <DetailsPage />
-          </Route>
+      <Sheet>
+        <div className="App">
+          <Typography variant="h1">The Movies Saga!</Typography>
+          <Router>
+            <Route path="/" exact>
+              <MovieList />
+            </Route>
+            <Route path="/details/:id">
+              <DetailsPage />
+            </Route>
 
-          {/* Add Movie page */}
-        </Router>
-      </div>
+            {/* Add Movie page */}
+          </Router>
+        </div>
+      </Sheet>
     </CssVarsProvider>
   )
 }
