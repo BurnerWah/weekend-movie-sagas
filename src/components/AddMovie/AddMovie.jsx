8f0f76738170b0a3702a292fe2eb87d433e2d671
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-10 13:42:05 -0600

    Added most of the code for the add movie form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,7 +1,34 @@
-import { Breadcrumbs, Typography } from '@mui/joy'
-import { Link } from 'react-router-dom'
+import {
+  Breadcrumbs,
+  Button,
+  ButtonGroup,
+  Input,
+  Option,
+  Select,
+  Textarea,
+  Typography,
+} from '@mui/joy'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useHistory } from 'react-router-dom'
 
 export default function AddMovie() {
+  /** @type {import('redux').Dispatch<Actions>} */
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const [title, setTitle] = useState('')
+  const [poster, setPoster] = useState('')
+  const [description, setDescription] = useState('')
+  const [genre, setGenre] = useState(/** @type {number?} */ (null))
+
+  const genres = useSelector((/** @type {State} */ store) => store.genres)
+  const isLoading = useSelector((/** @type {State} */ store) => store.isLoading)
+
+  useEffect(() => {
+    dispatch({ type: 'SAGA/GET_GENRES' })
+  }, [])
+
   return (
     <>
       <Breadcrumbs>
@@ -14,6 +41,45 @@ export default function AddMovie() {
         <Typography level="h2" sx={{ textAlign: 'center' }}>
           Add Movie
         </Typography>
+        <Input
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <Input
+          placeholder="Poster URL"
+          value={poster}
+          onChange={(e) => setPoster(e.target.value)}
+          required
+        />
+        <Textarea
+          minRows={3}
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <Select
+          value={genre}
+          onChange={(_, v) => setGenre(v)}
+          placeholder="Genre..."
+          required
+        >
+          {genres.map(({ id, name }) => (
+            <Option key={id} value={id}>
+              {name}
+            </Option>
+          ))}
+        </Select>
+        <ButtonGroup>
+          <Button color="danger" onClick={history.goBack}>
+            Cancel
+          </Button>
+          <Button color="primary" type="submit" loading={isLoading}>
+            Save
+          </Button>
+        </ButtonGroup>
       </form>
     </>
   )
