25a2f089d51e055725460c91eb220ebda2d68207
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-10 13:01:24 -0600

    Started adding things to the Add Movie page

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom'
 
 export default function AddMovie() {
   return (
-    <Breadcrumbs>
-      <Typography>
-        <Link to="/">Home</Link>
-      </Typography>
-      <Typography>Add Movie</Typography>
-    </Breadcrumbs>
+    <>
+      <Breadcrumbs>
+        <Typography>
+          <Link to="/">Home</Link>
+        </Typography>
+        <Typography>Add Movie</Typography>
+      </Breadcrumbs>
+      <form>
+        <Typography level="h2" sx={{ textAlign: 'center' }}>
+          Add Movie
+        </Typography>
+      </form>
+    </>
   )
 }
